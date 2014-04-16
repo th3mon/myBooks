2572f64bb9fc52adeda5e2a0b76eb16ceefaebf1
th3mon <p.szelenberger@gmail.com>
2014-04-17 01:06:30 +0200

    ADD: main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function (g, d, $, u){
+    var
+        init = function() {
+            var
+                title = $('title').text() + ' :: Przemysław SID Szelenberger';
+
+            $('title').text(title);
+        };
+    $(init);
+}(window, document, jQuery));
+    
